ee15f593afe807e7603219491ea5d5d92f0ae8e4
chp320 <chp320@naver.com>
2022-08-07 11:52:48 +0900

    [스프링 퀵 스타트] class4 의존성 주입 - 생성자 인젝션

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -19,6 +19,13 @@ public class SamsungTV implements TV {
         System.out.println("===> SamsungTV(1) 객체 생성");
     }
 
+    // '생성자 인젝션' 테스트를 위해 '매개변수 가지는 생성자' 생성
+    // SonySpeaker 객체를 매개변수로 받아서 멤버 변수로 선언된 참조변수 speaker를 초기화 한다.
+    public SamsungTV(SonySpeaker speaker) {
+        System.out.println("===> SamsungTV(2) 객체 생성");
+        this.speaker = speaker;
+    }
+
     public void powerOn() {
         System.out.println("SamsungTV---전원 켠다.");
     }
@@ -29,13 +36,13 @@ public class SamsungTV implements TV {
 
     public void volumeUp() {
 //        System.out.println("SamsungTV---소리 올린다.");
-        speaker = new SonySpeaker();
+//        speaker = new SonySpeaker();
         speaker.volumeUp();
     }
 
     public void volumeDown() {
 //        System.out.println("SamsungTV---소리 내린다.");
-        speaker = new SonySpeaker();
+//        speaker = new SonySpeaker();
         speaker.volumeDown();
     }
 }
