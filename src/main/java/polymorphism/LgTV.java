b2d07197ef52768b769908007cf95bf8f4721283
chp320 <chp320@naver.com>
2022-08-07 23:58:01 +0900

    [스프링 퀵 스타트] class5 어노테이션 기반 설정 : @Resource

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,14 +1,12 @@
 package polymorphism;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import javax.annotation.Resource;
 import org.springframework.stereotype.Component;
 
 @Component("tv")
 public class LgTV implements TV {
 
-    @Autowired
-    @Qualifier("apple")
+    @Resource(name="apple")
     private Speaker speaker;
 
     public LgTV() {
