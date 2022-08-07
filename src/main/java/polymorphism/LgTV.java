eaab4cbd3e1630b74b1203107a66df52d4ac3bcf
chp320 <chp320@naver.com>
2022-08-07 16:36:12 +0900

    [스프링 퀵 스타트] class5 어노테이션 기반 설정 : component-scan 설정

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,6 +1,14 @@
 package polymorphism;
 
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTV implements TV {
+
+    public LgTV() {
+        System.out.println("===> LgTV 객체 생성");
+    }
+
     @Override
     public void powerOn() {
         System.out.println("LgTV---전원 켠다.");
