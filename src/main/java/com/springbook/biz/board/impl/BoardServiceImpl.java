9eea53a6173cf1b0f567399b294e7301e63e4708
chp320 <chp320@naver.com>
2022-08-13 17:55:53 +0900

    [스프링 퀵 스타트] day2, class1 - BoardServiceImpl 모든 메서드에 LogAdvice 호출 로직 추가

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -2,6 +2,7 @@ package com.springbook.biz.board.impl;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.LogAdvice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,29 +16,39 @@ public class BoardServiceImpl implements BoardService {
     // DB CRUD 기능 처리 DAO 클래스 멤버, 의존성 주입을 위해 @Autowired 사용
     @Autowired
     private BoardDAO boardDAO;
+    private LogAdvice log;
+
+    public BoardServiceImpl() {
+        log = new LogAdvice();
+    }
 
     @Override
     public void insertBoard(BoardVO vo) {
+        log.printLog();
         boardDAO.insertBoard(vo);
     }
 
     @Override
     public void updateBoard(BoardVO vo) {
+        log.printLog();
         boardDAO.updateBoard(vo);
     }
 
     @Override
     public void deleteBoard(BoardVO vo) {
+        log.printLog();
         boardDAO.deleteBoard(vo);
     }
 
     @Override
     public BoardVO getBoard(BoardVO vo) {
+        log.printLog();
         return boardDAO.getBoard(vo);
     }
 
     @Override
     public List<BoardVO> getBoardList(BoardVO vo) {
+        log.printLog();
         return boardDAO.getBoardList(vo);
     }
 }
