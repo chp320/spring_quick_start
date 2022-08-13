39ea2d556eb60f07e80637c5ad243cb8ab57fa63
chp320 <chp320@naver.com>
2022-08-13 18:21:01 +0900

    [스프링 퀵 스타트] day2, class1 - 공통기능(log) 제거(원복)

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -17,46 +17,29 @@ public class BoardServiceImpl implements BoardService {
     // DB CRUD 기능 처리 DAO 클래스 멤버, 의존성 주입을 위해 @Autowired 사용
     @Autowired
     private BoardDAO boardDAO;
-//    private LogAdvice log;
-    private Log4jAdvice log;
-
-    public BoardServiceImpl() {
-//        log = new LogAdvice();
-        log = new Log4jAdvice();
-    }
 
     @Override
     public void insertBoard(BoardVO vo) {
-//        log.printLog();
-        log.printLogging();
         boardDAO.insertBoard(vo);
     }
 
     @Override
     public void updateBoard(BoardVO vo) {
-//        log.printLog();
-        log.printLogging();
         boardDAO.updateBoard(vo);
     }
 
     @Override
     public void deleteBoard(BoardVO vo) {
-//        log.printLog();
-        log.printLogging();
         boardDAO.deleteBoard(vo);
     }
 
     @Override
     public BoardVO getBoard(BoardVO vo) {
-//        log.printLog();
-        log.printLogging();
         return boardDAO.getBoard(vo);
     }
 
     @Override
     public List<BoardVO> getBoardList(BoardVO vo) {
-//        log.printLog();
-        log.printLogging();
         return boardDAO.getBoardList(vo);
     }
 }
