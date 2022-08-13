cd71fbf54f229b181285883e97b1524691013a3c
chp320 <chp320@naver.com>
2022-08-13 17:53:16 +0900

    [스프링 퀵 스타트] day2, class1 - 공통으로 처리할 로직 LogAdvice 작성

---
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -0,0 +1,10 @@
+package com.springbook.biz.common;
+
+/**
+ * 모든 비즈니스 메서드가 실행되기 직전에 공통으로 처리할 로직
+ */
+public class LogAdvice {
+    public void printLog() {
+        System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+    }
+}
