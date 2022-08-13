8fc8286e3a64ff5e73af87418170b7f5aa794080
chp320 <chp320@naver.com>
2022-08-13 17:28:15 +0900

    [스프링 퀵 스타트] class6 - 어노테이션으로 작성

--- src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
+++ src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -2,8 +2,12 @@ package com.springbook.biz.user.impl;
 
 import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service("userService")
 public class UserServiceImpl implements UserService {
+    @Autowired
     private UserDAO userDAO;
 
     // setter 인젝션으로 의존성 주입
