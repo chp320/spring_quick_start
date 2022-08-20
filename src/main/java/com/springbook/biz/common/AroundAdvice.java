8382f80daadaecce4cc73bb80baf181bf780fce5
chp320 <chp320@naver.com>
2022-08-21 04:38:35 +0900

    [스프링 퀵 스타트] DAY02, Class04 - JoinPoint 예제 (around)

--- src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,6 +1,7 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 /**
  * Around 어드바이스
@@ -8,10 +9,25 @@ import org.aspectj.lang.ProceedingJoinPoint;
  */
 public class AroundAdvice {
     public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
+        // (22.08.20) JoinPoint/바인드변수 테스트
+        /*
         System.out.println("[BEFORE]: 비즈니스 메서드 수행 전에 처리할 내용...");
         Object returnObj = pjp.proceed();   // proceed() 메서드를 통해서 비즈니스 메서드 호출 가능!
         System.out.println("[AFTER]: 비즈니스 메서드 수행 후에 처리할 내용...");
 
         return returnObj;
+        */
+
+        String method = pjp.getSignature().getName();
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object obj = pjp.proceed();
+
+        stopWatch.stop();
+        System.out.println(method + "() 메서드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
+
+        return obj;
     }
 }
