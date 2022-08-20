123125987cef51f2ffda517b441d70b569fd7bec
chp320 <chp320@naver.com>
2022-08-20 22:32:07 +0900

    [스프링 퀵 스타트] DAY02, Class04 - JoinPoint 예제

--- src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,10 +1,17 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 /**
  * 정의 : 포인트컷으로 지정된 메서드 호출 시, 메서드가 실행되기 전에 처리될 내용들을 기술하기 위해 사용
  */
 public class BeforeAdvice {
-    public void beforeLog() {
+    // (22.08.20) JoinPoint 객체 사용 -> JoinPoint 어드바이스 메서드 매개변수 선언
+//    public void beforeLog() {
+    public void beforeLog(JoinPoint jp) {
+        String method = jp.getSignature().getName();
+        Object[] args = jp.getArgs();
+
         System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
     }
 }
--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,10 +1,15 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 /**
  * 모든 비즈니스 메서드가 실행되기 직전에 공통으로 처리할 로직
  */
 public class LogAdvice {
-    public void printLog() {
+    // (22.08.20) JoinPoint 객체 사용 -> JoinPoint 어드바이스 메서드 매개변수 선언
+    // JoinPoint를 사용하는 이유 : 호출된 메서드 시그니처 (리턴타입, 이름, 매개변수) 정보를 얻을 수 있어 세부적인 로직 처리 가능하다.
+//    public void printLog() {
+    public void printLog(JoinPoint jp) {
         System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
     }
 }
