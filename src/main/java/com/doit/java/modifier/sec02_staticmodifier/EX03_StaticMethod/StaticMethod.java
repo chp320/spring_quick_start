1581cea48bf8aa249a4d1e1ba1fca2ea680618f9
chp320 <chp320@naver.com>
2022-08-28 21:09:04 +0900

    [Do it] 자바 완전 정복 - 9장. 접근 지정자1 : static 제어자

---
+++ src/main/java/com/doit/java/modifier/sec02_staticmodifier/EX03_StaticMethod/StaticMethod.java
@@ -0,0 +1,14 @@
+package com.doit.java.modifier.sec02_staticmodifier.EX03_StaticMethod;
+
+class A {
+    void abc() {
+        System.out.println("instance 메서드");
+    }
+
+    static void bcd() {
+        System.out.println("static 메서드");
+    }
+}
+
+public class StaticMethod {
+}
