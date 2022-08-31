05159f3bd7182294ae35259355aa2ea8e90c3f33
chp320 <chp320@naver.com>
2022-09-01 05:37:18 +0900

    [Do it] 자바 완전 정복 - 10장.클래스의 상속과 다형성 : 오버라이딩/오버로딩

---
+++ src/main/java/com/doit/java/inheritance/sec03_methodoverriding/EX03_MethodOverriding_3/MethodOverriding_3.java
@@ -0,0 +1,43 @@
+package com.doit.java.inheritance.sec03_methodoverriding.EX03_MethodOverriding_3;
+
+class A {
+    void print1() {
+        System.out.println("A 클래스 print1");
+    }
+    void print2() {
+        System.out.println("A 클래스 print2");
+    }
+}
+
+class B extends A {
+    @Override
+    void print1() {
+        System.out.println("B 클래스 print1");
+    }
+
+    void print2(int a) {
+        System.out.println("B 클래스 print2");
+    }
+}
+
+public class MethodOverriding_3 {
+    public static void main(String[] args) {
+        // A타입 선언 - A생성자 사용
+        A aa = new A();
+        aa.print1();
+        aa.print2();
+        System.out.println();
+
+        // B타입 선언 - B생성자 사용
+        B bb = new B();
+        bb.print1();
+        bb.print2();
+        bb.print2(3);
+        System.out.println();
+
+        // A타입 선언 - B생성자 사용
+        A ab = new B();
+        ab.print1();
+        ab.print2();
+    }
+}
