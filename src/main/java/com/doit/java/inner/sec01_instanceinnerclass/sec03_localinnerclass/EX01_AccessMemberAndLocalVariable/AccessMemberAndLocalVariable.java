9512c19d663deceab23044f3fbc0ae6ec4c9658d
chp320 <chp320@naver.com>
2022-09-22 00:32:21 +0900

    [Do it] 자바 완전 정복 - 13장.이너 클래스와 이너 인터페이스 : 지역 이너 클래스

---
+++ src/main/java/com/doit/java/inner/sec01_instanceinnerclass/sec03_localinnerclass/EX01_AccessMemberAndLocalVariable/AccessMemberAndLocalVariable.java
@@ -0,0 +1,24 @@
+package com.doit.java.inner.sec01_instanceinnerclass.sec03_localinnerclass.EX01_AccessMemberAndLocalVariable;
+
+/** 지역 이너 클래스 내부에서 외부 멤버 및 메서드 지역변수 활용 */
+
+class A {
+    int a = 3;      // 필드
+    void abc() {
+        int b = 5;  // 지역 변수
+        // 지역 이너 클래스 선언
+        class B {
+            void bcd() {
+                System.out.println(a);
+                System.out.println(b);
+                a = 5;
+            }
+        }
+        // 지역 이너 클래스 객체 생성
+        B bb = new B();
+        // 메서드 호출 -> 메모리에 로딩
+        bb.bcd();
+    }
+}
+public class AccessMemberAndLocalVariable {
+}
